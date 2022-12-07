5fa3780abfda90da608c9cd74a96810d299319bd
Tyson Hoekstra <tyejae@gmail.com>
2022-12-07 09:20:46 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,7 @@ const Request = require('request');
 client.login(process.env.BOT_TOKEN);
 
 // client.registry.registerGroup('msf.gg', 'MSG.GG');
-client.registry.registerGroup('raids', 'Raids');
 client.registry.registerGroup('admin', 'Admin');
-client.registry.registerGroup('fun', 'Fun');
 client.registry.registerCommandsIn(__dirname + "/commands");
 client.registry.registerDefaults();
 client.on('error', error => console.log(error));
